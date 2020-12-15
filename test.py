c7a7f41d83a2f65a5f3e65686a6fd95b1a35de1f
Jacob045 <jacob045@foxmail.com>
2020-12-15 11:54:48 +0800

    修改文件夹名称

--- test.py
+++ test.py
@@ -1,15 +1,4 @@
-
-import numpy as np
 import pandas as pd
-
-MSE_data = pd.DataFrame()
-
-print(MSE_data)
-
-row = 0
-while row < 3:
-    MSE_data.loc[0,row] = row
-    MSE_data.loc[1,row] = row +10
-    row = row + 1
-
-print(MSE_data)
+filepath = r'I:\Data\Personal Data\graduation project\SACOL\microwave\2008\lv2_200805.csv'
+data = pd.read_csv(filepath)
+print((data))
