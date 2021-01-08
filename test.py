bd0cb80ec071af0ce5323fe426c4ca3dab6c4462
Jacob045 <jacob045@foxmail.com>
2021-01-08 18:25:45 +0800

    更新

--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-import requests   
-from lxml import etree   
-url = "https://mofanpy.com/tutorials/data-manipulation/scraping/download/"  
-response = requests.get(url).content   
-html = etree.HTML(response)
-b = html.xpath('/html/body/main/div[2]/div[4]/div[1]/p[4]/a/img')  
-print(b) 
+from tqdm.std import trange
+for i in trange(1000000):
+    pass
